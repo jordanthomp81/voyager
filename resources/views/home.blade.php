e255133058904ca99fce62ee53707355c3fd750b
Jordan <jordanthomp81@gmail.com>
2018-01-21 16:28:10 -0600

    added some routes;

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -77,19 +77,7 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <img style="position: absolute; top: 0px; left: 50%; transform: translateX(-50%);" src="https://image.ibb.co/cy78Yb/voyager.png" width="1600px" height="auto" alt="">
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,61 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+});
+
+Route::get('/create-user', function () {
+    return view('create-user');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/dashboard-new-user', function () {
+    return view('dashboard-new-user');
+});
+
+Route::get('/settings', function () {
+    return view('settings');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/projects/{projectId}', function ($projectId) {
+  return view('projects/individual-project');
+});
+
+Route::get('/tasks', function () {
+    return view('tasks');
+});
+
+Route::get('/tasks/{taskId}', function ($taskId) {
+    return view('tasks/individual-task');
+});
+
+Route::get('/messages/{messageId}', function ($messageId) {
+    return view('messages');
+});
+
+Route::get('/media', function () {
+    return view('media');
+});
+
+Route::get('/media/{mediaId}', function ($mediaId) {
+    return view('media/individual-media-item');
+});
+
+Route::get('/team', function () {
+    return view('team');
 });
